de934c346e1702b8f09c7b0951edb3ef65d7c3a2
jamesnogra@gmail <jamesnogra@gmail.com>
2024-09-29 11:29:05 +0800

    Add a readme to install and use the project.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Installation
+
+- Clone the repository using `git clone https://github.com/jamesnogra/smollm-test.git`
+- Go to the directory of the cloned project `cd smollm-test`
+- Install the required packages `pip install transformers accelerate sentencepiece`
+
+# Running the project
+
+- Run the command `python app.py`
+- Open a browser are go to http://127.0.0.1:5000/ to start
--- model.py
+++ model.py
@@ -2,8 +2,12 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 
 model_name = "KhantKyaw/T5-small_new_chatbot"
 
+print(f'Loading the model {model_name}')
+
 tokenizer = T5Tokenizer.from_pretrained(model_name)
 model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+print(f'Done loading the model')
 
 def generate_response(input_text):
   input_ids = tokenizer.encode(input_text, return_tensors='pt')
