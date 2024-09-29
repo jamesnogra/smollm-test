a8e581001e934e1462361c39019cdfd89819025e
jamesnogra <jamesnogra@gmail.com>
2024-09-29 02:46:18 +0000

    First test code for the SmolLM.

---
+++ chat.py
@@ -0,0 +1,14 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+model_name = "KhantKyaw/T5-small_new_chatbot"
+
+tokenizer = T5Tokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+def generate_response(input_text):
+  input_ids = tokenizer.encode(input_text, return_tensors='pt')
+  outputs = model.generate(input_ids, min_length=5, max_length=512, do_sample=True, num_beams=10, no_repeat_ngram_size=2)
+  generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+  return generated_text
+
+prompt = 'hello'
+print(generate_response(prompt))
