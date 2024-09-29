a13d3789d51719ee3ecd97ea79f3ce1708a380fa
jamesnogra@gmail <jamesnogra@gmail.com>
2024-09-29 12:01:00 +0800

    Reduce number of tokens for performance improvements.

--- model.py
+++ model.py
@@ -11,6 +11,6 @@ print(f'Done loading the model')
 
 def generate_response(input_text):
   input_ids = tokenizer.encode(input_text, return_tensors='pt')
-  outputs = model.generate(input_ids, min_length=5, max_length=128, do_sample=True, num_beams=10, no_repeat_ngram_size=2)
+  outputs = model.generate(input_ids, min_length=8, max_length=64, do_sample=True, num_beams=10, no_repeat_ngram_size=2)
   generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
   return generated_text
